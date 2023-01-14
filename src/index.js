3882bb04523901ea4d3bc4e36fb0ca9abc443f45
mariamzakariadze <mariazakariadze@gmail.com>
2023-01-14 12:21:08 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "../components/App.jsx"
+import index from './index.css'
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <>
+    <App />
+  </>
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js"
+  },
+  devServer: {
+    port: 8080,
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: './build'
+    },
+
+  },
+  module: {
+    rules: [
+        {
+          test: /\.jsx?/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          // use: ['babel-loader'],
+          options: {
+            presets: ['@babel/env', '@babel/react'],
+          },
+        },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-inline-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: 'index.html',
+      filename: 'index.html'
+    }),
+  ]
+};
